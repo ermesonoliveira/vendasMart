2f1326da893baff7cf6a827944bfe49738d84ef7
Ermeson <ermesonoliveira16@hotmail.com>
2016-10-21 01:43:56 -0200

    meu primeiro commit

---
+++ src/main/java/vendasMart/Principal.java
@@ -0,0 +1,11 @@
+package vendasMart;
+
+public class Principal {
+	public static void main(String[] args) {
+		System.out.println("hello world");
+		int i;
+		i=0;
+		System.out.println(i);
+	}
+
+}
